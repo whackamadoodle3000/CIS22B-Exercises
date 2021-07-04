2ad0361a9151e355d8a2e616bfd4099a965fcd68
Pranav Tadepalli <orca.pranav@gmail.com>
2021-07-04 01:58:15 -0700

    Update day4.cpp

--- day4.cpp
+++ day4.cpp
@@ -25,11 +25,18 @@ int deleteZeros(int array[], int size);
 /**
     Reverse the case of the cstring
     @param letters the c string
+    @return number of letters changed
 */
 int reverseCase(char letters[]);
 
 
-getNumList
+/**
+    Creates an array of numofnums numbers
+    @param numofnums number of integers to read
+    @return address of array of numbers
+*/
+int* getNumList(int size);
+
 int main(){
     // test deleteZeros
     int test[] = {10, 0,  20, 30, 40, 0, 50};
@@ -47,17 +54,30 @@ int main(){
     for (char letter:letters){
         cout << letter;
     }
-    cout << endl;
+    cout << endl << endl;
+    
+    //test getNumList
+    int* pointer;
+    pointer = getNumList(3);
+    
+    for (int i=0; i<3; i++, pointer++){
+        cout << *(&pointer) << ",";
+    }
+    cout << endl << endl;
+    
     return 0;
 }
 
 //Shift the contents of each element in the array whenever a zero element is deleted
 int deleteZeros(int array[], int size){
     int zeroes = 0;
-    for (int i=0;i<size-1;i++){
-        if (array[i]==0){
-            for (int j=i; j<size;j++){
-                array[j] = array[j+1];
+    int* pointerOuter = array;
+    int* pointerInner;
+    for (int i=0;i<size-1;i++,pointerOuter++){
+        if (*pointerOuter==0){
+            pointerInner = pointerOuter;
+            for (int j=i; j<size;j++,pointerInner++){
+                array[j] = *(pointerInner+1);
             }
             zeroes++;
         }
@@ -69,20 +89,30 @@ int deleteZeros(int array[], int size){
 //Reverse the case of the cstring
 int reverseCase(char letters[]){
     int numberChanged = 0;
-    for (int i=0; i<strlen(letters); i++){
+    char* letterPointer = letters;
+    for (int i=0; i<strlen(letters); i++, letterPointer++){
         if (islower(letters[i])){
-            letters[i] = toupper(letters[i]);
+            letters[i] = toupper(*letterPointer);
             numberChanged++;
         }
         else if (isupper(letters[i])){
-            letters[i] = tolower(letters[i]);
+            letters[i] = tolower(*letterPointer);
             numberChanged++;
         }
     }
     return numberChanged;
 }
 
-
+int* getNumList(int size){
+    int numlist[size];
+    int* pointer = numlist;
+    int tempNum;
+    for (int i = 0; i<size; i++, pointer++){
+        cout << "Enter an Integer: ";
+        cin >> numlist[i];
+    }
+    return &(numlist[0]);
+}
 
 
 
